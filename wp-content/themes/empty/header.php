5348924e0b217e2cdf9a0e8c39fd8a9d21abeb78
josefcarlsson <josef.carlsson@me.com>
2015-08-24 16:28:17 +0200

    new version

--- wp-content/themes/empty/header.php
+++ wp-content/themes/empty/header.php
@@ -18,10 +18,20 @@
 	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Cardo:400,400italic,700' rel='stylesheet' type='text/css'>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fitvids.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/instafeed.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/js.js"></script>
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
 </head>
+<!------------------------------- MENU ------------------------------>
+<nav>
+	<img src="<?php echo get_template_directory_uri(); ?>/images/actionist.png" alt="actionist">
+	<ul>
+		<li>JOBB</li>
+		<li>OM OSS</li>
+		<li>KONTAKTA OSS</li>
+		<li><span class="dott"></span><span class="dott"></span></li>
+	</ul>
+</nav>
--- wp-content/themes/empty/start.php
+++ wp-content/themes/empty/start.php
@@ -23,8 +23,26 @@ get_header(); ?>
 
 <!DOCTYPE html>
 
+<!------------------------------- presentation ------------------------------>
+
+<script>
+	var quats = []
+	window.quats= quats
+</script>
+	
+<?php if( have_rows('quats') ): 
+	 while( have_rows('quats') ): the_row(); 
+		 $quat= get_sub_field('quat');?>
+		<script>
+			var obj = {text: '<?php echo $quat ?>'}
+			quats.push(obj);
+			console.log(quats)
+		</script>
+	<?php endwhile;
+endif; ?>
+
+
 <section id="top" class="fullHeight">
-	<h1>fkdofklödsfklö</h1>
 	<video width="100%" height="100%" autoplay="" muted="" id="video" loop="">
 		<source src="<?php echo get_template_directory_uri(); ?>/images/movie.mp4" type="video/mp4">
 		<source src="<?php echo get_template_directory_uri(); ?>/images/movie.ogv" type="video/ogv">
@@ -33,20 +51,10 @@ get_header(); ?>
 	<div class="verticalWrapper">
 		<div class="withWrapper">
 			<h3><span id="timeOfDay"></span></h3>
-			<h1><span>Vi är: </h1><h1 id="quats"></span></h1>
+			<h1><span>Vi är:</span><span id="quats"></span></h1>
 	</div>
 	</div>
 </section>
-<!------------------------------- MENU ------------------------------>
-<nav>
-	<img src="<?php echo get_template_directory_uri(); ?>/images/actionist.png" alt="actionist">
-	<ul>
-		<li>JOBB</li>
-		<li>OM OSS</li>
-		<li>KONTAKTA OSS</li>
-		<li><span class="dott"></span><span class="dott"></span></li>
-	</ul>
-</nav>
 
 <!------------------------------- presentation ------------------------------>
 <section id="presentation" class="presentation withWrapper">
@@ -58,7 +66,7 @@ get_header(); ?>
 
 <!------------------------------- case ------------------------------>
 
-<div class="case">
+<section id="case">
 	<?php query_posts( array ( 'category_name' => 'cases', 'posts_per_page' => -1 ) ); ?>
 	<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); 
 		 if(get_field('showOnStart') == 'Ja') : ?>
@@ -68,23 +76,80 @@ get_header(); ?>
 		<?php endif;
 			endwhile; 
 	endif; ?>
-</div>
+</section>
 
 <!------------------------------- news ------------------------------>	
-<section class="news">
+<section id="news">
 	<h3>Ha koll på</h3>
 	<h1>Livet på Actionsit</h1>
-	<div class="">
 	<?php query_posts( array ( 'category_name' => 'news', 'posts_per_page' => -1 ) ); ?>
-	<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); 		
-			if(get_field('showOnStart') == 'Ja') : ?>
-			<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-				<?php get_template_part( 'content', 'page' );?>		
-			</a>
-		<?php endif; ?>
-	<?php endwhile; ?>
+		<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); 	?>	
+				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+					<?php get_template_part( 'content', 'usp' );?>		
+				</a>
+				<?php endwhile; ?>
 	<?php endif; ?>
 </section>
 
 <!------------------------------- uspar ------------------------------>
-<?php get_footer(); ?>
+<section id="uspar" class="group section">
+	<div class="uspNewsWrapper col span_4_of_12">
+		<?php query_posts( array ( 'category_name' => 'news', 'posts_per_page' => -1 ) ); ?>
+			<?php if (have_posts()) : 
+				 while (have_posts()) : the_post(); 	?>	
+				<?php if(get_field('showOnStart') != 'Ja') : ?>
+					<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+						<?php get_template_part( 'content', 'usp' );?>		
+					</a>
+				<?php else : ?>
+			<?php endif; ?>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+	
+	<div class="span_4_of_12 col instagram" id="instafeed">
+	</div>
+	
+	<div class="span_4_of_12 col spotify">
+		<h2>Spotify</h2>
+		<?php 
+			$username = 'femhundratolv';
+			$scrobbler_url = "http://ws.audioscrobbler.com/2.0/user/" . $username . "/recenttracks";
+			
+			if ($scrobbler_xml = file_get_contents($scrobbler_url)) {
+			       $scrobbler_data = simplexml_load_string($scrobbler_xml);        
+			       $i=0;
+			       echo '<ul>';
+			       foreach ($scrobbler_data->track as $track) {
+			          $string = '<li>';
+			          if($track->image[3]){
+			              $string .= '<div class="cover"><img width="300" height="300" class="cover" src="' . $track->image[3] . '" /></div>';
+			              }
+			          else{
+			          $string .= '<div class="cover"><img width="300" height="300" class="cover" src="fdsfds" /></div>';
+			          }
+			               $string .= '<p><span class="title">' . $track->artist . '</span><br />' . $track->name . '</p>';
+			               $string .= '<p>Played: ' . $track->date . '</p>';
+			               $string .= '</li>';
+			               echo $string;
+			               $i++;
+			if($i==1) break;
+			       }
+			       echo '</ul>';
+			}
+		?>
+	</div>
+</section>
+
+<section id="clients">
+	fdsa
+	<?php if( have_rows('clientsLogos') ): 
+	 while( have_rows('clientsLogos') ): the_row(); 
+		 $logo= get_sub_field('logo');?>
+		 	<div class="span_3_of_12 col logo">
+			 	<img src="<?php echo $logo ?>">
+		 	</div>
+		<?php endwhile;
+	endif; ?>		
+</section>
+
